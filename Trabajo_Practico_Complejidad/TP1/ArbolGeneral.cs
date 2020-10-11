559bb33f4e851f730c65c626e42add63f7ff0d90
iodenKisuka <iodenteken@hotmail.com>
2020-10-11 15:42:40 -0300

    ejercicio Ancho

--- Trabajo_Practico_Complejidad/TP1/ArbolGeneral.cs
+++ Trabajo_Practico_Complejidad/TP1/ArbolGeneral.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using tp1;
 
 namespace tp1
 {
@@ -34,13 +35,59 @@ namespace tp1
 		}
 	
 		public int altura() {
-			return 0;
+			int alturaM = 0;
+			if (esHoja()) { return alturaM; }
+			else
+			{
+
+
+
+
+			}
+
+
 		}
 	
 		
 		public int nivel(T dato) {
 			return 0;
 		}
-	
+
+		// Ejercicio 4 C  TP1
+		public int ancho()
+		{
+			int ancho = 1;
+			List<ArbolGeneral<T>> hijosdelArbol = hijos;
+			List<ArbolGeneral<T>> Siguiente_nivel_hijos;
+			do
+			{
+				if (ancho < hijos.Count)
+				{
+					ancho = hijos.Count;
+				}
+				Siguiente_nivel_hijos = new List<ArbolGeneral<T>>();
+				foreach (ArbolGeneral<T> Obtener_hijo in hijos)
+				{
+					foreach (ArbolGeneral<T> hijo in Obtener_hijo.getHijos())
+					{
+						Siguiente_nivel_hijos.Add(hijo);
+					}
+				}
+				hijos = Siguiente_nivel_hijos;
+
+			} while (Siguiente_nivel_hijos.Count > 0);
+
+
+
+			return ancho;
+		}
+
+
+
+
+
+
 	}
 }
+
+
